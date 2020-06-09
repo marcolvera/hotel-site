2aa8ede5eb37242f2b3f0d648e4a93cfe97b751a
Marco Olvera <marcoolvera@macbook-pro.home>
2020-06-09 16:54:05 -0700

    add switch routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
 
 
 
 function App() {
   return (
-    <Home />
+    <div>
+      <Switch>
+        <Route exact path = '/home' render = {() =>
+          <Home />
+        }/>
+        <Route exact path = '/about' render = {() =>
+          <About />
+        }/>
+        <Route exact path = '/contact' render = {() =>
+          <Contact />
+        }/>
+      </Switch>
+    </div>
   );
 }
 
