7a89040395fc5f28c5990a5729f86e201893a6fb
Marco Olvera <marcoolvera@macbook-pro.home>
2020-06-09 16:46:31 -0700

    create contact page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
+
 
 
 function App() {
---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Contact.css';
+import NavBar from '../../components/NavBar/NavBar';
+
+const Contact = () => {
+    return (
+        <NavBar />
+    )
+}
+
+export default Contact;
