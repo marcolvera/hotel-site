51655795d3c6a08ba25058aa4c8f43666e63fa8e
Marco Olvera <marcoolvera@macbook-pro.home>
2020-06-09 16:48:17 -0700

    create about page

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './About.css';
+import NavBar from '../../components/NavBar/NavBar';
+
+const About = () => {
+    return (
+        <NavBar />
+    )
+}
+
+export default About;
