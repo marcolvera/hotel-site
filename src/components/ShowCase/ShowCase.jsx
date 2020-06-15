54c48f0591ccfc0dfc508dd434232469464098ba
Marco Olvera <marcoolvera@MacBook-Pro.lan1>
2020-06-15 11:24:00 -0700

    add text and image to showcase

--- src/components/ShowCase/ShowCase.jsx
+++ src/components/ShowCase/ShowCase.jsx
@@ -5,8 +5,13 @@ const ShowCase = () => {
     return (
         <div id='showContainer'>
             <h1><span>Enjoy</span> Your Stay</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut tempore voluptas, 
+            eligendi ad ducimus expedita quidem quasi aliquam id? Distinctio!
+            </p>
         </div>
     )
 }
 
 export default ShowCase;
+       
+        
