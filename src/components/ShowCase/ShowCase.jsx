590f7b825ec4fe858b227f13c016b12662979743
Marco Olvera <marcoolvera@MacBook-Pro.lan1>
2020-06-15 11:04:42 -0700

    create showcase component

---
+++ src/components/ShowCase/ShowCase.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ShowCase.css';
+
+const ShowCase = () => {
+    return (
+        <div id='showContainer'>
+            <h1><span>Enjoy</span> Your Stay</h1>
+        </div>
+    )
+}
+
+export default ShowCase;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Home.css';
 import NavBar from '../../components/NavBar/NavBar';
+import ShowCase from '../../components/ShowCase/ShowCase';
 
 const Home = () => {
     return (
         <div>
             <NavBar />
+            <ShowCase />
         </div>
     )
 }
