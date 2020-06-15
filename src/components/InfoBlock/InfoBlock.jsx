d2fbff49c3525fc0c3a45300f15c78c6cc87c68f
Marco Olvera <marcoolvera@MacBook-Pro.lan1>
2020-06-15 13:17:26 -0700

    create InfoBlock component

---
+++ src/components/InfoBlock/InfoBlock.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './InfoBlock.module.css';
+
+const InfoBlock = () => {
+    return(
+        <div><h1>Hello World</h1></div>
+    )
+};
+
+export default InfoBlock;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import ShowCase from '../../components/ShowCase/ShowCase';
+import InfoBlock from '../../components/InfoBlock/InfoBlock';
 
 const Home = () => {
     return (
         <div>
             <NavBar />
             <ShowCase />
+            <InfoBlock />
         </div>
     )
 }
