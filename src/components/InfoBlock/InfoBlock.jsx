efc9a53d000c7be7cd881010a5ddc5b8181ea17c
Marco Olvera <marcoolvera@MacBook-Pro.lan1>
2020-06-16 11:24:56 -0700

    adjust box sizing

--- src/components/InfoBlock/InfoBlock.jsx
+++ src/components/InfoBlock/InfoBlock.jsx
@@ -5,11 +5,23 @@ const InfoBlock = () => {
     return(
         <div>
             <div className={styles.container}>
-                <div className={styles.block1}>Hello World</div>
-                <div className={styles.block2}>Hello World</div>
+                <div className={styles.block1}> 
+                    <img src="https://i.imgur.com/f69CzXA.png" alt=""/>
+                </div>
+                <div className={styles.block2}>
+                    <div className={styles.content}>
+                        <h4><span>Find a Price</span> Right for You</h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis iure ducimus optio ratione culpa 
+                            aliquam iusto tempore adipisci provident nemo modi, 
+                            eum necessitatibus tenetur fuga suscipit rem accusamus nulla corrupti?
+                        </p>
+                        <div><h5>Learn More</h5></div>
+                    </div>
+                </div>
             </div>
         </div>
     )
 };
+
 
 export default InfoBlock;
