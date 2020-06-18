178cb275f7c5ffdd08fb56c83a36e70f60b3cf7d
Marco Olvera <marcoolvera@MacBook-Pro.lan1>
2020-06-18 13:07:43 -0700

    create placelist comp

---
+++ src/components/PlaceList/PlaceList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './PlaceList.module.css';
+
+const PlaceList = () => {
+    return(
+        <div><h1>Hello World</h1></div>
+    )
+};
+
+export default PlaceList;
--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './Explore.css';
 import NavBar from '../../components/NavBar/NavBar';
 import Map from '../../components/Map/Map';
+import PlaceList from '../../components/PlaceList/PlaceList';
 
 const Contact = () => {
     return (
         <div>
             <NavBar />
             <Map />
+            <PlaceList />
         </div>
     )
 }
