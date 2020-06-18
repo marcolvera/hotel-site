0c00745adbe5923b2eeaa690a0a9f1a12163842a
Marco Olvera <marcoolvera@MacBook-Pro.lan1>
2020-06-18 13:02:40 -0700

    add the rest of boxes

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,11 +4,23 @@ import styles from './Map.module.css';
 const Map = () => {
     return(
         <div>
-            <h1 className={styles.h1}>Find Your Next Adventure</h1>
-            <div className={styles.img}>
-                <div className={styles.box}>
+            <h1 className={styles.h1}><span>Find Your Next</span> Adventure</h1>
+            <div className={styles.container}>
+                <div className={styles.box1}>
                     <div className={styles.content}>
                         <h3>Get Inspired</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, ipsa.</p>
+                    </div>
+                </div>
+                <div className={styles.box2}>
+                    <div className={styles.content}>
+                        <h3>Plan Ahead</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, ipsa.</p>
+                    </div>
+                </div>
+                <div className={styles.box3}>
+                    <div className={styles.content}>
+                        <h3>Have Fun</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, ipsa.</p>
                     </div>
                 </div>
