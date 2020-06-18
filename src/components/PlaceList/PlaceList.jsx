175a279271f5d24088938e3f46c7f13c8af3577d
Marco Olvera <marcoolvera@MacBook-Pro.lan1>
2020-06-18 13:23:55 -0700

    create rows

--- src/components/PlaceList/PlaceList.jsx
+++ src/components/PlaceList/PlaceList.jsx
@@ -3,7 +3,23 @@ import styles from './PlaceList.module.css';
 
 const PlaceList = () => {
     return(
-        <div><h1>Hello World</h1></div>
+        <div>
+            <h1 className={styles.h1}>Explore Destinations</h1>
+            <div className={styles.container}>
+                <div className={styles.rows}>
+                    <h4>Hello World</h4>
+                    <h4>Hello World</h4>
+                    <h4>Hello World</h4>
+                    <h4>Hello World</h4>
+                </div>
+                <div className={styles.rows}>
+                    <h4>Hello World</h4>
+                    <h4>Hello World</h4>
+                    <h4>Hello World</h4>
+                    <h4>Hello World</h4>
+                </div>
+            </div>
+        </div>
     )
 };
 
